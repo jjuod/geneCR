c21eaff2190ff6de0845168bbd3564f30929745e
jjuod <j.juodakis@gmail.com>
2022-09-26 16:06:12 +0200

    binary & cont meta test

--- pipeline.sh
+++ pipeline.sh
@@ -30,19 +30,22 @@ fi
 
 # OPTION B: sim1000G. See snakefile
 
-
 # ---------------
 # run the R script to simulate phenotype
 # ./simulate-pheno.R
 
 # analyze via plink
-~/soft/plink1.9/plink --bfile 1000g/simulated_cm \
+~/soft/plink1.9/plink --bfile 1000g/simulated \
 	--pheno simphenos/cont.pheno --assoc \
 	--out simphenos/res_cont
+~/soft/plink1.9/plink --bfile 1000g/simulated \
+	--pheno simphenos/bin.pheno --logistic beta \
+	--out simphenos/res_bin
 
 
 # using t scores as z scores and fake alleles
 awk 'NR==1{print "SNP", "A1", "A2", "N", "Z"; next} {print $2, "A1", "A2", $4, $8}' simphenos/res_cont.qassoc > simphenos/res_cont.ldready
+awk 'NR==1{print "SNP", "A1", "A2", "N", "Z"; next} {print $2, "A1", "A2", $6, $8}' simphenos/res_bin.assoc.logistic > simphenos/res_bin.ldready
 
 # run LD score regr
 conda activate ldsc
@@ -50,3 +53,17 @@ conda activate ldsc
 	--ref-ld 1000g/simulated_ldscores \
 	--w-ld 1000g/simulated_ldscores \
 	--out simphenos/ldsc
+
+./ldsc/ldsc.py --h2 simphenos/res_bin.ldready \
+	--ref-ld 1000g/simulated_ldscores \
+	--w-ld 1000g/simulated_ldscores \
+	--samp-prev 0.10 --pop-prev 0.10 \
+	--out simphenos/ldscb
+# this kind of works and gives similar results.
+
+# and this, blindly metaanalysing lin and log betas, gives worse results:
+./ldsc/ldsc.py --h2 simphenos/res_meta.ldready \
+	--ref-ld 1000g/simulated_ldscores \
+	--w-ld 1000g/simulated_ldscores \
+	--out simphenos/ldscm
+
--- simulate-pheno.R
+++ simulate-pheno.R
@@ -2,7 +2,7 @@ library(bigsnpr)
 
 setwd("~/Documents/results/cr/")
 
-bed = snp_readBed("1000g/simulated_cm.bed")
+bed = snp_readBed("1000g/simulated.bed")
 bed = snp_attach(bed)
 y = snp_simuPheno(bed$genotypes, h2=0.3, M=2000)
 
@@ -10,3 +10,26 @@ newfam = bed$fam[,1:2]
 newfam$Y = y$pheno
 
 write.table(newfam, file="simphenos/cont.pheno", quote=F, sep="\t", col.names=F, row.names=F)
+
+K = 0.10
+
+newfam$Y = as.numeric(newfam$Y>quantile(newfam$Y, 1-K))+1
+write.table(newfam, file="simphenos/bin.pheno", quote=F, sep="\t", col.names=F, row.names=F)
+
+
+# meta-analyse blindly
+dflog = read.table("simphenos/res_bin.assoc.logistic",h=T)
+dflin = read.table("simphenos/res_cont.qassoc", h=T)
+
+dflin$W = 1/dflin$SE^2
+dflog$W = 1/(dflog$BETA/dflog$STAT)^2  # z=beta/SE => SE=beta/z
+
+# Estimates the same as:
+# metafor::rma(yi=c(dflin$BETA[1], dflog$BETA[1]), sei=c(dflin$SE[1], dflog$BETA[1]/dflog$STAT[1]), method="FE")
+metab = (dflin$BETA*dflin$W + dflog$BETA*dflog$W)/(dflin$W+dflog$W)
+metase = sqrt(1/(dflin$W + dflog$W))
+metaz = metab/metase
+
+meta = data.frame(SNP=dflin$SNP, A1="A1", A2="A2", N=4000, Z=metaz)
+
+write.table(meta, file="simphenos/res_meta.ldready", quote=F, row.names=F)
