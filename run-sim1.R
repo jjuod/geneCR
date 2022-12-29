d4e1734999de129b42e738ef63638629579d6888
jjuod <j.juodakis@gmail.com>
2022-12-29 17:45:51 +1300

    looping sim1 over sigma

--- run-sim1.R
+++ run-sim1.R
@@ -42,70 +42,73 @@ crres = tibble()
 
 NITER = 500
 
-for(i in 1:NITER){
-  print(i)
-  causal = sample_n(gff2[,c("gene", "snps")], NCAUSALGENES)
-  causalgenes = unique(causal$gene)
-  obsgenes = list()
-  
-  for(run in c(1, 2)){
-    causal$geneeff = rnorm(nrow(causal), 0, 0.5)
-    causalsnps = unnest(causal, snps)
-    causalsnps$snpeff = rnorm(nrow(causalsnps), causalsnps$geneeff, 0.5)
+for(sigma in c(30, 40)){
+  print(paste("------ sigma:", sigma, " -------------"))
+  for(i in 1:NITER){
+    print(i)
+    causal = sample_n(gff2[,c("gene", "snps")], NCAUSALGENES)
+    causalgenes = unique(causal$gene)
+    obsgenes = list()
     
-    # simulate phenos
-    xbeta = NULL
-    ids = NULL
-    bfile = NULL
-    if(run==1){
-      xbeta = as.numeric(causalsnps$snpeff %*% gt3000[causalsnps$snps+6,1:2000])
-      ids = 1:2000
-      bfile = "simulated_3000"
-    } else {
-      xbeta = as.numeric(causalsnps$snpeff %*% gt2000[causalsnps$snps+6,1:2000])
-      ids = 1:2000
-      bfile = "simulated_2000"
+    for(run in c(1, 2)){
+      causal$geneeff = rnorm(nrow(causal), 0, 0.5)
+      causalsnps = unnest(causal, snps)
+      causalsnps$snpeff = rnorm(nrow(causalsnps), causalsnps$geneeff, 0.5)
+      
+      # simulate phenos
+      xbeta = NULL
+      ids = NULL
+      bfile = NULL
+      if(run==1){
+        xbeta = as.numeric(causalsnps$snpeff %*% gt3000[causalsnps$snps+6,1:2000])
+        ids = 1:2000
+        bfile = "simulated_3000"
+      } else {
+        xbeta = as.numeric(causalsnps$snpeff %*% gt2000[causalsnps$snps+6,1:2000])
+        ids = 1:2000
+        bfile = "simulated_2000"
+      }
+      ys = xbeta + rnorm(length(xbeta), 0, 40)
+      
+      ressnps = runGWAS(ys, ids, bfile)
+      foundgenes = snpstogenes$gene[ressnps]
+      foundgenes = unique(foundgenes[!is.na(foundgenes)])
+      obsgenes[[length(obsgenes)+1]] = foundgenes
+      
+      # store heritability etc:
+      newrow = data.frame(h2s=var(xbeta)/var(ys), numsignhits=length(ressnps),
+                          numsigngenes=length(foundgenes), run=run, i=i)
+      summaries = bind_rows(summaries, newrow)
     }
-    ys = xbeta + rnorm(length(xbeta), 0, 30)
+    # print(summaries)
     
-    ressnps = runGWAS(ys, ids, bfile)
-    foundgenes = snpstogenes$gene[ressnps]
-    foundgenes = unique(foundgenes[!is.na(foundgenes)])
-    obsgenes[[length(obsgenes)+1]] = foundgenes
+    # Processing for C-R:
+    listall = unique(unlist(obsgenes))
+    capt.hist = sapply(obsgenes, function(x) as.numeric(listall %in% x))
+    # print(capt.hist)
     
-    # store heritability etc:
-    newrow = data.frame(h2s=var(xbeta)/var(ys), numsignhits=length(ressnps),
-                        numsigngenes=length(foundgenes), run=run, i=i)
-    summaries = bind_rows(summaries, newrow)
-  }
-  # print(summaries)
-  
-  # Processing for C-R:
-  listall = unique(unlist(obsgenes))
-  capt.hist = sapply(obsgenes, function(x) as.numeric(listall %in% x))
-  # print(capt.hist)
-  
-  # Run C-R + handle exceptions:  
-  if(length(listall)==1){
-    # mark NAs if only one gene reported
-    mod = data.frame(abundance=NA, stderr=NA, deviance=NA, df=NA, AIC=NA, BIC=NA, infoFit=NA)
-  } else {
-    mod = data.frame(closedp.0(capt.hist)$results)
-    # mark NAs in no-overlap cases:
-    if(mod$abundance[1]>1e7){
-      mod$abundance = NA
-      mod$stderr = NA
-    }  
+    # Run C-R + handle exceptions:  
+    if(length(listall)==1){
+      # mark NAs if only one gene reported
+      mod = data.frame(abundance=NA, stderr=NA, deviance=NA, df=NA, AIC=NA, BIC=NA, infoFit=NA)
+    } else {
+      mod = data.frame(closedp.0(capt.hist)$results)
+      # mark NAs in no-overlap cases:
+      if(mod$abundance[1]>1e7){
+        mod$abundance = NA
+        mod$stderr = NA
+      }  
+    }
+    
+    crres = bind_rows(crres, mod)
   }
   
-  crres = bind_rows(crres, mod)
+  write.table(crres, paste0("crres_2000x2_s", sigma, ".csv"), sep="\t", quote=F, col.names=T, row.names=F)
+  write.table(summaries, paste0("studysummaries_2000x2_s", sigma, ".csv"), sep="\t", quote=F, col.names=T, row.names=F)
 }
 
-write.table(crres, "crres_2000x2.csv", sep="\t", quote=F, col.names=T, row.names=F)
-write.table(summaries, "studysummaries_2000x2.csv", sep="\t", quote=F, col.names=T, row.names=F)
-
-crres = read.table("crres_2000x2.csv", h=T)
-summaries = read.table("studysummaries_2000x2.csv", h=T)
+crres = read.table("crres_2000x2_s40.csv", h=T)
+summaries = read.table("studysummaries_2000x2_s40.csv", h=T)
 
 summaries
 crres
